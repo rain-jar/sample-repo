6c6b5582e4e53dc8bf4120f9ce88cd8504c951e3
rajrnair90 <raj.nair@colorado.edu>
2020-08-04 08:31:54 -0400

    Third commit

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
 
 
 def greet(who_to_greet):
@@ -18,5 +11,5 @@ print(greet("Raj"))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-print("Hello World")
+print(r.ok)
 
